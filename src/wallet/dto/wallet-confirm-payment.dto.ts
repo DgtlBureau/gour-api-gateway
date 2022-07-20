10d2a68297e7208340a10ccf177e864adf256713
Artyom Vorokhta <avorohta@newshift.ru>
2022-07-20 13:16:43 +0300

    wallet controller

---
+++ src/wallet/dto/wallet-confirm-payment.dto.ts
@@ -0,0 +1,16 @@
+import { IsEnum, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { WalletTransactionStatus } from '../../common/dto/wallet-transaction.dto';
+
+export class WalletConfirmPaymentDto {
+  @ApiProperty({
+    enum: WalletTransactionStatus,
+    default: WalletTransactionStatus.init,
+  })
+  @IsEnum(WalletTransactionStatus)
+  status: WalletTransactionStatus;
+
+  @ApiProperty()
+  @IsString()
+  token: string;
+}
