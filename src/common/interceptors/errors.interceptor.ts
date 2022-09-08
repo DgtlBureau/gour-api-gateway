1fd98df30a113d442672e57d6c0450232e2a0f67
MirageN1349 <desertirpipi@gmail.com>
2022-09-08 14:36:01 +0300

    fix: error interceptor and sentry

--- src/common/interceptors/errors.interceptor.ts
+++ src/common/interceptors/errors.interceptor.ts
@@ -13,9 +13,12 @@ export class ErrorsInterceptor implements NestInterceptor {
   intercept(_: ExecutionContext, next: CallHandler): Observable<unknown> {
     return next.handle().pipe(
       catchError((err) => {
-        return throwError(
-          () => new HttpException(err.message, err.getStatus()),
-        );
+        if (err instanceof HttpException) {
+          return throwError(
+            () => new HttpException(err.message, err.getStatus()),
+          );
+        }
+        return throwError(() => new HttpException(err?.message, err?.status));
       }),
     );
   }
