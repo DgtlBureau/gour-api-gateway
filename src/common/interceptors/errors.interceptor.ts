ed6d81de1dda43dd989ebe2228a6f957da90fd3e
MirageN1349 <desertirpipi@gmail.com>
2022-09-08 13:06:53 +0300

    fix: instanseof для error interceptor

--- src/common/interceptors/errors.interceptor.ts
+++ src/common/interceptors/errors.interceptor.ts
@@ -10,13 +10,14 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
-    return next
-      .handle()
-      .pipe(
-        catchError((err) =>
-          throwError(() => new HttpException(err.message, err.status)),
-        ),
-      );
+  intercept(_: ExecutionContext, next: CallHandler): Observable<unknown> {
+    return next.handle().pipe(
+      catchError((err) => {
+        if (err instanceof HttpException) {
+          return throwError(() => err);
+        }
+        return throwError(() => new HttpException(err.message, err.status));
+      }),
+    );
   }
 }
