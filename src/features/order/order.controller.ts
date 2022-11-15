e2f287d7280864c3d201b4499707d2f4e4d0b31d
MirageN1349 <desertirpipi@gmail.com>
2022-11-15 22:11:16 +0300

    feat: 🎸 добавил оплату конкретного заказа

--- src/features/order/order.controller.ts
+++ src/features/order/order.controller.ts
@@ -10,11 +10,17 @@ import {
   Post,
   Put,
   Query,
+  Redirect,
   Res,
   UseGuards,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Response } from 'express';
 import { firstValueFrom } from 'rxjs';
 
@@ -27,9 +33,10 @@ import { OrderDto } from '../../common/dto/order.dto';
 import { TOTAL_COUNT_HEADER } from '../../constants/httpConstants';
 import { AuthGuard } from '../../common/guards/auth.guard';
 import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+import { PayDto } from '../payment/dto/pay.dto';
+import { InvoiceDto } from 'src/common/dto/invoice.dto';
 
 @ApiBearerAuth()
-@UseGuards(AuthGuard)
 @ApiTags('orders')
 @Controller('orders')
 export class OrderController {
@@ -42,6 +49,7 @@ export class OrderController {
   @ApiResponse({
     type: [OrderResponseDto],
   })
+  @UseGuards(AuthGuard)
   @Get('/')
   async getAll(
     @CurrentUser() client: ClientDto,
@@ -58,9 +66,23 @@ export class OrderController {
     return res.send(orders);
   }
 
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.PERMANENT_REDIRECT)
+  @Redirect()
+  @Get('/change-order-status-by-token')
+  async changeOrderStatusByToken(@Query('authToken') token: string) {
+    const data = await firstValueFrom(
+      this.client.send('change-order-status-by-token', token),
+    );
+    return {
+      url: data.redirect,
+    };
+  }
+
   @ApiResponse({
     type: OrderDto,
   })
+  @UseGuards(AuthGuard)
   @Get('/:id')
   getOne(@Param('id') id: string) {
     return this.client.send('get-order', +id);
@@ -69,25 +91,38 @@ export class OrderController {
   @ApiResponse({
     type: OrderDto,
   })
+  @UseGuards(AuthGuard)
   @Post('/')
   post(@CurrentUser() client: ClientDto, @Body() dto: OrderCreateDto) {
     return this.client.send('create-order', { dto, client });
   }
 
   @ApiResponse({
+    type: InvoiceDto,
+  })
+  @UseGuards(AuthGuard)
+  @Post('/pay-order')
+  payOrder(@Body() dto: PayDto) {
+    return this.client.send('pay-order', dto);
+  }
+
+  @ApiResponse({
     type: OrderDto,
   })
+  @UseGuards(AuthGuard)
   @Put('/:id')
   put(@Param('id') id: string, @Body() dto: Partial<OrderDto>) {
     return this.client.send('edit-order', { id: +id, dto });
   }
 
   @Delete('/:id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.client.send('delete-order', +id);
   }
 
   @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
   @Post('/refresh-order-status')
   updateOrderStatus(@Body() dto: string) {
     const parsedDto: UpdateOrderStatusDto = JSON.parse(JSON.stringify(dto));
