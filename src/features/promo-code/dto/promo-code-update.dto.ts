38c6939297260264ecac17204ccbd67bb3f464c9
Artyom Vorokhta <avorohta@newshift.ru>
2022-12-06 13:03:06 +0300

    feat: 🎸 promocodes

---
+++ src/features/promo-code/dto/promo-code-update.dto.ts
@@ -0,0 +1,40 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsDateString,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class PromoCodeUpdateDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  key?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  discount?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsDateString()
+  end?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  totalCount?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  countForOne?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsArray()
+  categoryIds?: number[];
+}
