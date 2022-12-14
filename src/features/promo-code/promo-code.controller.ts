c89612e9fc7100d2813666fa172a4728d9d46da9
Artyom Vorokhta <avorohta@newshift.ru>
2022-12-14 15:56:12 +0300

    feat: 🎸 products, promo codes and referrals export

--- src/features/promo-code/promo-code.controller.ts
+++ src/features/promo-code/promo-code.controller.ts
@@ -17,8 +17,8 @@ import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ClientProxy } from '@nestjs/microservices';
 import { Response } from 'express';
 import { firstValueFrom } from 'rxjs';
+import * as XLSX from 'xlsx';
 
-import { BaseGetListDto } from '../../common/dto/base-get-list.dto';
 import { PromoCodeDto } from '../../common/dto/promo-code.dto';
 import { PromoCodeCreateDto } from './dto/promo-code-create.dto';
 import { PromoCodeUpdateDto } from './dto/promo-code-update.dto';
@@ -27,16 +27,19 @@ import { TOTAL_COUNT_HEADER } from '../../constants/httpConstants';
 import { PromoCodeCheckDto } from './dto/promo-code-check.dto';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { ClientDto } from 'src/common/dto/client.dto';
+import { makeBook } from 'src/common/utils/xlsxUtil';
+import { BaseGetListDto } from 'src/common/dto/base-get-list.dto';
+import { ExportDto } from 'src/common/dto/export.dto';
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard)
 @ApiTags('promo-codes')
 @Controller('promo-codes')
 export class PromoCodeController {
-  constructor(@Inject('MAIN_SERVICE') private mainClient: ClientProxy) {}
+  constructor(@Inject('MAIN_SERVICE') private client: ClientProxy) {}
 
   async onModuleInit() {
-    await this.mainClient.connect();
+    await this.client.connect();
   }
 
   @ApiOkResponse({
@@ -46,7 +49,7 @@ export class PromoCodeController {
   @Get('/')
   async getAll(@Query() params: BaseGetListDto, @Res() res: Response) {
     const [promoCodes, count] = await firstValueFrom(
-      this.mainClient.send('get-promo-codes', params),
+      this.client.send('get-promo-codes', params),
       { defaultValue: [[], 0] },
     );
 
@@ -59,10 +62,54 @@ export class PromoCodeController {
     type: PromoCodeDto,
   })
   @HttpCode(HttpStatus.OK)
+  @Post('/apply')
+  check(@Body() dto: PromoCodeCheckDto, @CurrentUser() client: ClientDto) {
+    return this.client.send('apply-promo-code', {
+      dto,
+      clientId: client.id,
+    });
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/export')
+  async export(
+    @Query() params: BaseGetListDto,
+    @Body() dto: ExportDto,
+    @Res() res: Response,
+  ) {
+    const [promoCodes, _count] = await firstValueFrom(
+      this.client.send('get-promo-codes', { params, dto }),
+      { defaultValue: null },
+    );
+
+    const wb = this.makePromoCodesBook(promoCodes);
+
+    const listDate = new Date().toLocaleDateString();
+
+    const fileName = `promo_codes_list_${listDate}`;
+
+    res.set({
+      'Content-Type':
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Disposition': `attachment; filename="${fileName}.xlsx"`,
+    });
+
+    const promoCodesReport = XLSX.write(wb, {
+      type: 'buffer',
+      bookType: 'xlsx',
+    });
+
+    return res.send(promoCodesReport);
+  }
+
+  @ApiOkResponse({
+    type: PromoCodeDto,
+  })
+  @HttpCode(HttpStatus.OK)
   @Get('/:id')
   async getOne(@Param('id') id: string, @Res() res: Response) {
     const promoСode = await firstValueFrom(
-      this.mainClient.send('get-promo-code', +id),
+      this.client.send('get-promo-code', +id),
       { defaultValue: null },
     );
 
@@ -72,22 +119,10 @@ export class PromoCodeController {
   @ApiOkResponse({
     type: PromoCodeDto,
   })
-  @HttpCode(HttpStatus.OK)
-  @Post('/apply')
-  check(@Body() dto: PromoCodeCheckDto, @CurrentUser() client: ClientDto) {
-    return this.mainClient.send('apply-promo-code', {
-      dto,
-      clientId: client.id,
-    });
-  }
-
-  @ApiOkResponse({
-    type: PromoCodeDto,
-  })
   @HttpCode(HttpStatus.CREATED)
   @Post('/')
   post(@Body() dto: PromoCodeCreateDto) {
-    return this.mainClient.send('create-promo-code', { dto });
+    return this.client.send('create-promo-code', { dto });
   }
 
   @ApiOkResponse({
@@ -96,12 +131,47 @@ export class PromoCodeController {
   @HttpCode(HttpStatus.OK)
   @Put('/:id')
   put(@Param('id') id: string, @Body() dto: PromoCodeUpdateDto) {
-    return this.mainClient.send('edit-promo-code', { id: +id, dto });
+    return this.client.send('edit-promo-code', { id: +id, dto });
   }
 
   @HttpCode(HttpStatus.OK)
   @Delete('/:id')
   remove(@Param('id') id: string) {
-    return this.mainClient.send('delete-promo-code', +id);
+    return this.client.send('delete-promo-code', +id);
+  }
+
+  makePromoCodesBook(promoCodes: PromoCodeDto[]) {
+    const titles = [
+      'Код',
+      'Скидка',
+      'Общее кол-во',
+      'Кол-во для одного',
+      'Категории',
+      'Кол-во использований',
+    ];
+
+    const rows = promoCodes.map(
+      ({ key, discount, totalCount, countForOne, categories, orders }) => {
+        const countOfUses = orders.length.toString();
+        const categoriesList = categories
+          .map((category) => category.title.ru)
+          .join(', ');
+
+        const row = [
+          key,
+          discount.toString(),
+          totalCount.toString(),
+          countForOne.toString(),
+          categoriesList,
+          countOfUses,
+        ];
+
+        return row;
+      },
+    );
+
+    const wb = makeBook(titles, rows);
+
+    return wb;
   }
 }
