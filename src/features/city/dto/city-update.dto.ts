5d4ca32325ef0c4b59642aa1fdf364f42804e24a
Sergey Smolkov <damager4425@gmail.com>
2023-01-09 14:46:52 +0300

    feat: стоимость доставки у городов

--- src/features/city/dto/city-update.dto.ts
+++ src/features/city/dto/city-update.dto.ts
@@ -1,11 +1,16 @@
 import { TranslatableStringCreateDto } from '../../../common/dto/translatable-string-create.dto';
 import { Type } from 'class-transformer';
-import { ValidateNested } from 'class-validator';
+import { ValidateNested, IsOptional, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CityUpdateDto {
   @ValidateNested()
+  @IsOptional()
   @Type(() => TranslatableStringCreateDto)
   @ApiProperty()
   name: TranslatableStringCreateDto;
+
+  @IsOptional()
+  @IsNumber()
+  deliveryCost: number;
 }
